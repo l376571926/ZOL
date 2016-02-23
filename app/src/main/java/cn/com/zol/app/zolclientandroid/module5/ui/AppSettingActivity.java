e7c606fd4d39348409001021b73dec6e1ae0bd71
l376571926 <376571926@qq.com>
2016-02-23 15:29:45 +0800

    wxq:模块五也没写什么，加了点东西

--- app/src/main/java/cn/com/zol/app/zolclientandroid/module5/ui/AppSettingActivity.java
+++ app/src/main/java/cn/com/zol/app/zolclientandroid/module5/ui/AppSettingActivity.java
@@ -2,18 +2,19 @@ package cn.com.zol.app.zolclientandroid.module5.ui;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.logging.Logger;
 
 import cn.com.zol.app.zolclientandroid.R;
 import cn.com.zol.app.zolclientandroid.module5.utils.DataCleanManager;
@@ -22,12 +23,20 @@ import cn.com.zol.app.zolclientandroid.other.utils.ToolBarUtils;
 public class AppSettingActivity extends Activity implements View.OnClickListener{
 
     private TextView settingBack;
-    private RelativeLayout setTextSixe;
+    private RelativeLayout setTextSize;
     private TextView getTextSize;
     private RelativeLayout nightMode;
     private TextView screenBrightness;
     private RelativeLayout setCache;
     private TextView cacheClean;
+    private RelativeLayout setThirdParty;
+    private TextView imageMode;
+    private RelativeLayout setImageQuality;
+    private CheckBox setRightScroll;
+    private CheckBox setDisplayImg;
+    private CheckBox setPullArticle;
+    private CheckBox setpullNotifi;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +49,19 @@ public class AppSettingActivity extends Activity implements View.OnClickListener
 
     private void initView() {
         settingBack = (TextView) findViewById(R.id.setting_back);
-        setTextSixe = (RelativeLayout) findViewById(R.id.set_text_size);
+        setTextSize = (RelativeLayout) findViewById(R.id.set_text_size);
         getTextSize = (TextView) findViewById(R.id.get_text_size);
         nightMode = (RelativeLayout) findViewById(R.id.set_night_mode);
         screenBrightness = (TextView) findViewById(R.id.set_screen_brightness);
         setCache = (RelativeLayout) findViewById(R.id.set_cache);
         cacheClean = (TextView) findViewById(R.id.cache_clean);
+        setThirdParty = (RelativeLayout) findViewById(R.id.set_third_party);
+        imageMode = (TextView) findViewById(R.id.image_mode);
+        setImageQuality = (RelativeLayout) findViewById(R.id.set_image_quality);
+        setRightScroll = (CheckBox) findViewById(R.id.set_right_scroll);
+        setDisplayImg = (CheckBox) findViewById(R.id.set_display_img);
+        setPullArticle = (CheckBox) findViewById(R.id.set_pull_article);
+        setpullNotifi = (CheckBox) findViewById(R.id.set_pull_notification);
 
         reflashCache();
     }
@@ -54,9 +70,35 @@ public class AppSettingActivity extends Activity implements View.OnClickListener
 
     private void initEvent() {
         settingBack.setOnClickListener(this);
-        setTextSixe.setOnClickListener(this);
+        setTextSize.setOnClickListener(this);
         nightMode.setOnClickListener(this);
         setCache.setOnClickListener(this);
+        setThirdParty.setOnClickListener(this);
+        setImageQuality.setOnClickListener(this);
+        setRightScroll.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(AppSettingActivity.this,"未定义设置",Toast.LENGTH_SHORT).show();
+            }
+        });
+        setDisplayImg.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(AppSettingActivity.this,"未定义设置",Toast.LENGTH_SHORT).show();
+            }
+        });
+        setPullArticle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(AppSettingActivity.this,"未定义设置",Toast.LENGTH_SHORT).show();
+            }
+        });
+        setpullNotifi.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(AppSettingActivity.this,"未定义设置",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void initData() {
@@ -78,6 +120,12 @@ public class AppSettingActivity extends Activity implements View.OnClickListener
             case R.id.set_cache:
                 DataCleanManager.clearAllCache(this);
                 reflashCache();
+                break;
+            case R.id.set_third_party:
+                jump(ThirdPartyActivity.class);
+                break;
+            case R.id.set_image_quality:
+                showImageQualityDislog();
                 break;
         }
     }
@@ -104,6 +152,31 @@ public class AppSettingActivity extends Activity implements View.OnClickListener
                         break;
                     case 3:
                         getTextSize.setText("小");
+                        break;
+                }
+            }
+        });
+        builder.show();
+    }
+
+    /**
+     * 设置更改App的整画面质量的弹出对话框（还未设置成调整全部）
+     */
+    private void showImageQualityDislog(){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(AppSettingActivity.this);
+        builder.setTitle("画面质量");
+        final String[] str = new String[]{"高品质","低品质"};
+        builder.setItems(str, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case 0:
+                        imageMode.setText("高品质");
+                        break;
+                    case 1:
+                        imageMode.setText("低品质");
+                        break;
+                    default:
                         break;
                 }
             }
@@ -141,4 +214,15 @@ public class AppSettingActivity extends Activity implements View.OnClickListener
             e.printStackTrace();
         }
     }
+
+    /**
+     * active的无数据跳转
+     * @param cls 指定的active
+     */
+    private void jump(Class cls){
+        Intent intent = new Intent(this,cls);
+        startActivity(intent);
+    }
+
+
 }
